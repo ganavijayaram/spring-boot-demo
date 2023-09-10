bdd8fa404ad6d941027e41afc47ec766ee3d2e85
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-09 22:59:33 -0400

    Handling request params

--- springboot-rest-api/src/main/java/com/springboot/springbootrestapi/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/springboot/springbootrestapi/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.springboot.springbootrestapi.controller;
 import com.springboot.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -36,6 +37,7 @@ public class StudentController {
     }
 
     // Handling path variables
+    //http://localhost:8080/students/1/Ganavi/Jayaram
     @GetMapping("/students/{id}/{firstName}/{lastName}")
     public Student studentPathVariable(@PathVariable("id") int studentId,
                                        @PathVariable String firstName,
@@ -43,5 +45,15 @@ public class StudentController {
 
         return new Student(studentId,firstName, lastName);
     }
+
+    //using requestParam
+    //http://localhost:8080/students/query?id=1&firstName=GANAVI&lastName=JAYARAM
+    @GetMapping("/students/query")
+    public Student studentRequestParan(@RequestParam int id,
+                                       @RequestParam String firstName,
+                                       @RequestParam String lastName){
+        return new Student(id, firstName, lastName);
+    }
+
 
 }
